9b08222873f2da4461d920f41c8ea4c2625cc9f2
Lazermanna <60344938+Lazermanna@users.noreply.github.com>
2020-01-27 17:45:43 +0100

    OnFaitDesTests
    c'est un projet de merde fait pour le lol

---
+++ Assets/FilleFolleGeneratorBehaviour.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FilleFolleGeneratorBehaviour: MonoBehaviour
+{
+    public InputField premier, deuxieme;
+    string texteInitial, texteFinalAAfficher;
+
+    public List<string> listeDesLettres = new List<string>();
+    public List<string> listeDesCodes = new List<string>();
+
+    void Start()
+    {
+        print(listeDesLettres.Count);
+    }
+
+    void Update()
+    {
+        texteInitial = premier.text;
+
+        deuxieme.text = FaireUnTexteDeFilleFolleVersTexteNormal(texteInitial);
+    }
+
+    string SelectionnerUneSuiteDeMotsRandom(int nombreDeMots, List<string> laListeDeMots)
+    {
+        string phrasefinale = "";
+
+        for (int i = 0; i < nombreDeMots; i++)
+        {
+            phrasefinale += laListeDeMots[Random.Range(0, laListeDeMots.Count)];
+        }
+
+        return phrasefinale;
+    }
+
+    string FaireUnTexteDeFilleFolleVersTexteNormal(string texteEntre)
+    {
+        List<string> listeDesCaracteresEntres = new List<string>();
+
+        string phraseFinale = "";
+
+        foreach (var item in listeDesCodes)
+        {
+            if (texteEntre.Contains(item))
+            {
+                listeDesCaracteresEntres.Add(listeDesLettres[listeDesCodes.IndexOf(item)]);
+            }
+            if (texteEntre.Contains(" "))
+            {
+                listeDesCaracteresEntres.Add(" ");
+            }
+        }
+
+        for (int i = 0; i < listeDesCaracteresEntres.Count; i++)
+        {
+            phraseFinale += listeDesCaracteresEntres[i];
+        }
+
+        print("phrase finale :" + phraseFinale);
+        return phraseFinale;
+    }
+}
---
+++ Assets/FilleFolleRandomSelecterBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FilleFolleRandomSelecterBehaviour : MonoBehaviour
+{
+    public Text texteFinalAAfficher;
+    public int nombreMin, nombreMax;
+
+    public List<string> listeDesMots = new List<string>();
+
+    void Start()
+    {
+        print(listeDesMots.Count);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            texteFinalAAfficher.text = SelectionnerUneSuiteDeMotsRandom(Random.Range(nombreMin, nombreMax), listeDesMots);
+        }
+    }
+
+    string SelectionnerUneSuiteDeMotsRandom(int nombreDeMots, List<string> laListeDeMots)
+    {
+        string phrasefinale = "";
+
+        for (int i = 0; i < nombreDeMots; i++)
+        {
+            phrasefinale += laListeDeMots[Random.Range(0, laListeDeMots.Count)];
+        }
+
+        return phrasefinale;
+    }
+}
